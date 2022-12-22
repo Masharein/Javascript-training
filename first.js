e39285c79e3d6df5f9db4ed4c7ca960f23460254
Masharein <Masharein04061995@gmail.com>
2022-12-22 23:48:23 +0300

    new

--- first.js
+++ first.js
@@ -1,2 +1,43 @@
-let myName = 'Masha'
-console.log(myName)
+// const person = {
+//     name: 'Bob',
+//     age: 25
+// }
+// person.city = 'SPB'
+// // const person2 = Object.assign({}, person)
+// // const person2 = { ...person}
+// const person2 = JSON.parse(JSON.stringify(person))
+// person2.age = 26
+// person2.isAdult = true
+
+// console.log(person)
+
+// let a = 10
+// let b = 15
+// function sum(a, b) {
+//     const c = a + b
+//     console.log(c)
+// }
+// sum(a, b)
+// a = 18
+// b = 32
+// sum(a, b)
+
+// function myFn(a, b) {
+//     let c
+//     a = a + 1
+//     c = a + b
+//     return c 
+// }
+// myFn(a, b)
+
+const personOne = {
+    name: 'Bob',
+    age: 25
+}
+function increasePersonAge(person) {
+    personOne.age += 1
+    return person
+}
+
+increasePersonAge(personOne)
+console.log(personOne)
